53b2a1b078dd3296a84f1def81f1ce0e30cc0472
gelatini <gelatini92@gmail.com>
2026-02-07 16:22:56 -0500

    help.js fix

--- commands/help.js
+++ commands/help.js
@@ -7,20 +7,6 @@ const {
 const fs = require('fs');
 const path = require('path');
 
-const commandsPath = path.join(__dirname, '../'); // ajusta ruta
-const categories = fs.readdirSync(commandsPath);
-
-let totalCommands = 0;
-
-for (const category of categories) {
-  const files = fs.readdirSync(`${commandsPath}/${category}`)
-    .filter(file => file.endsWith('.js'));
-  totalCommands += files.length;
-}
-
-const totalCategories = categories.length;
-
-
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('help')
@@ -28,42 +14,40 @@ module.exports = {
 
   async execute(interaction) {
 
+    await interaction.deferReply(); // 🔥 evita timeout
+
+    const commandsPath = path.join(__dirname, '../../commands'); // AJUSTA ESTA RUTA
+    const categories = fs.readdirSync(commandsPath);
+
+    let totalCommands = 0;
+
+    for (const category of categories) {
+      const files = fs.readdirSync(`${commandsPath}/${category}`)
+        .filter(file => file.endsWith('.js'));
+      totalCommands += files.length;
+    }
+
     const embed = new EmbedBuilder()
       .setTitle('📖 | Menú de ayuda')
       .setColor('#313e59')
-      .setDescription(`Tengo **${totalCategories} categorías** y **${totalCommands} comandos** disponibles. Selecciona una categoría para ver sus comandos.`)
+      .setDescription(`Tengo **${categories.length} categorías** y **${totalCommands} comandos** disponibles.\nSelecciona una categoría para ver sus comandos.`)
       .setFooter({ text: 'Zeph • desarrollada por ♱ - Parra' });
 
     const menu = new StringSelectMenuBuilder()
       .setCustomId('help_menu')
       .setPlaceholder('Categorías')
       .addOptions([
-        {
-          label: '🎊 Entretenimiento',
-          description: 'Comandos de Entretenimiento',
-          value: 'fun',
-        },
-        {
-          label: '🔩 Moderación',
-          description: 'Comandos de moderación',
-          value: 'mod',
-        },
-        {
-          label: '🔎 Utilidad',
-          description: 'Comandos útiles',
-          value: 'utils',
-        },
-        {
-          label: '📀 Música',
-          description: 'Comandos de música',
-          value: 'music',
-        },
+        { label: '🎊 Entretenimiento', description: 'Comandos de diversión', value: 'fun' },
+        { label: '🔩 Moderación', description: 'Comandos de staff', value: 'mod' },
+        { label: '🔎 Utilidad', description: 'Comandos útiles', value: 'utils' },
+        { label: '📀 Música', description: 'Comandos de música', value: 'music' },
       ]);
 
     const row = new ActionRowBuilder().addComponents(menu);
 
-    await interaction.reply({ embeds: [embed], components: [row] });
+    await interaction.editReply({ embeds: [embed], components: [row] });
   }
 };
+
 
 
